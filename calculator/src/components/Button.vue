7ec2d87e3d5bc492fa149dc8362c1d559e613ccb
Néfi Fernandes <nefifernandes14@yahoo.com.br>
2021-09-27 16:48:46 -0300

    Add files via upload

---
+++ calculator/src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <buton class="button">
+        0
+    </buton>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {},
+        operation: {type: Boolean},
+        double: {type: Boolean},
+    }
+}
+</script>
+
+<style>
+.button{
+    background: #fff;
+    color: black;
+
+}
+</style>
